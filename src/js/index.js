83afc8d0047bfe6de9186c5af3e8567eb60b9c8a
RadovSlava <radov.slava@gmail.com>
2019-02-28 21:07:07 +0200

    1commit

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+var age = 18;
+console.log("my age: ", age);
+console.log("age has type: ", typeof age);
+
+var name = "vitali";
+console.log("my name: ", name);
+console.log("name has type: ", typeof name);
