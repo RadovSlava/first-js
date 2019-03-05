73bd44561bb4ed504249bba52905659e7f8b807f
RadovSlava <radov.slava@gmail.com>
2019-03-05 09:05:12 +0200

    2commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,33 @@
-var age = 18;
-console.log("my age: ", age);
-console.log("age has type: ", typeof age);
+var str = "string";
+console.log("str has type: ", typeof str);
 
-var name = "vitali";
-console.log("my name: ", name);
-console.log("name has type: ", typeof name);
+var weight = '80kg'
+console.log("weight has type: ", typeof true);
+
+var height = '187cm'
+console.log("height has type: ", typeof null);
+
+var speed = 'max'
+console.log("speed has type: ", typeof undefined);
+
+var name = 'Slava'
+console.log("name has type: ", typeof {});
+
+var n = '100'
+console.log("n has type: ", typeof 100);
+
+  var myCurrentAge = 33;
+
+  if (myCurrentAge => 25) {
+    console.log("I am over 25 years old");
+  } else if (myCurrentAge < 25) {
+    console.log("I am under 25");
+ }
+
+ var mycurrentage = prompt('?', 33);
+
+ var message = (mycurrentage < 25) ? console.log("I am under 25"):
+
+   (mycurrentage > 25) ? console.log("I am over 25 years old") :
+
+ alert( message );
